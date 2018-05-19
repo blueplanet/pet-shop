a93a557b6f3e208da51d5c9038ac9ec558b87e06
blueplanet <gyo@re-work.io>
2018-05-19 15:36:00 +0900

    add test

---
+++ test/TestAdoption.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.22;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Adoption.sol";
+
+contract TestAdoption {
+  Adoption adoption = Adoption(DeployedAddresses.Adoption());
+
+  function testUserCanAdoptPet() public {
+    uint returnedld = adoption.adopt(8);
+    uint expected = 8;
+    Assert.equal(returnedld, expected, 'Adoption of pet ID 8 should be recorded.');
+  }
+
+  function testGetAdopeterAddressByPetId() public {
+    address expected = this;
+    address adopter = adoption.adopters(8);
+    Assert.equal(adopter, expected, 'Owner of pet ID 8 should be recorded.');
+  }
+
+  function testGetAdopterAddressByPetIdInArray() public {
+    address expected = this;
+    address[16] memory adopters = adoption.getAdopters();
+    Assert.equal(adopters[8], expected, 'Owner of pet ID 8 should be recorded.');
+  }
+}
